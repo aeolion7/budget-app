99ac49feb09215194627658cf800e12b6d5d5a1d
Joseph Lionarons <lionsamurai@gmail.com>
2018-10-19 22:26:27 -0400

    added input reading js

--- app.js
+++ app.js
@@ -7,15 +7,37 @@ const budgetController = (function () {
 
 // UIController
 const UIController = (function () {
+  // DOMstrings object stores strings accessed by querySelector
+  const DOMstrings = {
+    inputButton: '.add__btn',
+    inputDescription: '.add__description',
+    inputType: '.add__type',
+    inputValue: '.add__value'
+  };
 
+  return {
+    getInput: function () {
+      return {
+        // either income or expenses
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMStrings: function () {
+      return DOMstrings;
+    }
+  };
 })();
 
 // global app controller
 const controller = (function (budgetCtrl, UICtrl) {
+  const DOM = UICtrl.getDOMStrings();
 
   const ctrlAddItem = function () {
     // get field input data
-
+    const input = UICtrl.getInput();
+    console.log(input);
     // add item to budget controller
 
     // add new item to UI
@@ -23,10 +45,9 @@ const controller = (function (budgetCtrl, UICtrl) {
     // calculate budget
 
     // display budget
-
   };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function (event) {
     // used both keyCode and which for browser compatibility
